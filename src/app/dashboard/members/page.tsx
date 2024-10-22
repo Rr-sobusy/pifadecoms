7d6cd03705bcd3c15c6b9ea504c789640a7860fd
unknown <rockph0029@gmail.com>
2024-10-22 16:20:53 +0800

    model changes

---
+++ src/app/dashboard/members/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return (
+    <Box>
+      <h2>rex</h2>
+    </Box>
+  );
+};
+
+export default Page;
