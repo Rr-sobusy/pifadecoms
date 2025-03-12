e64b8c07ae42cfd57af8fc0db8c99058ba7e3439
unknown <rockph0029@gmail.com>
2025-03-12 09:24:51 +0800

    changes

--- src/app/dashboard/settings/layout.tsx
+++ src/app/dashboard/settings/layout.tsx
@@ -10,18 +10,21 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps): React.JSX.Element {
   return (
-    <Box
-      sx={{
-        maxWidth: 'var(--Content-maxWidth)',
-        m: 'var(--Content-margin)',
-        p: 'var(--Content-padding)',
-        width: 'var(--Content-width)',
-      }}
-    >
-      <Stack direction={{ xs: 'column', md: 'row' }} spacing={4} sx={{ position: 'relative' }}>
-        <SideNav />
-        <Box sx={{ flex: '1 1 auto', minWidth: 0 }}>{children}</Box>
-      </Stack>
+    // <Box
+    //   sx={{
+    //     maxWidth: 'var(--Content-maxWidth)',
+    //     m: 'var(--Content-margin)',
+    //     p: 'var(--Content-padding)',
+    //     width: 'var(--Content-width)',
+    //   }}
+    // >
+    //   <Stack direction={{ xs: 'column', md: 'row' }} spacing={4} sx={{ position: 'relative' }}>
+    //     <SideNav />
+    //     <Box sx={{ flex: '1 1 auto', minWidth: 0 }}>{children}</Box>
+    //   </Stack>
+    // </Box>
+    <Box>
+      Construction on-going.
     </Box>
   );
 }
