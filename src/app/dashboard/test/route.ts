c36e6f4f589cc89b540aba932263e545f1832de2
unknown <rockph0029@gmail.com>
2025-05-06 10:54:32 +0800

    fix linter

--- src/app/dashboard/test/route.ts
+++ src/app/dashboard/test/route.ts
@@ -1,7 +1,7 @@
 import { stringify } from 'json-bigint';
 
 import { fetchMonthlyIncomeAndExpense } from '@/actions/overview/fetch-income-expense-by-month-current';
-import { fetchDoubleEntryPosted } from '@/actions/reports/account-transactions/fetch-double-entry-posted';
+// import { fetchDoubleEntryPosted } from '@/actions/reports/account-transactions/fetch-double-entry-posted';
 
 export async function GET() {
   //   const doublePosted = await fetchDoubleEntryPosted(new Date('2025-01-01'), new Date('2025-01-31'));
