12ba50a012cfd46a3d178bac4a0c8a258b0ca769
unknown <rockph0029@gmail.com>
2025-05-28 14:54:19 +0800

    fix linters

--- src/app/dashboard/reports/aging-invoice/page.tsx
+++ src/app/dashboard/reports/aging-invoice/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import AgingInvoiceTable from '@/components/dashboard/reports/aging-invoice/aging-invoice-table'
 import { fetchAgingInvoiceItemsPerMember } from '@/actions/invoices/aging-invoice'
-interface PageProps {}
 
-async function page({}: PageProps) {
+
+async function page() {
     const test = await fetchAgingInvoiceItemsPerMember()
   return (
     <AgingInvoiceTable data={test} />
